cb9fad85f83304b761bbfcc30693d5e941607ce6
Samyak-Sheth <shethsamy@outlook.com>
2023-06-14 14:36:12 +0200

    External Force

--- main.py
+++ main.py
@@ -7,14 +7,23 @@ import numpy as np
 import sympy as sp
 import faulthandler
 faulthandler.enable()
+from scipy.integrate import quad
 
 
 robot_urdf = "kuka_lbr_iiwa_support/urdf/lbr_iiwa_14_r820.urdf"
 
 
 
-import pybullet as p
-import numpy as np
+
+
+# Define the function to integrate
+def integrand(a,b,q, q_dot, torque_tot, C, r):
+    # Define the expression for the integrand based on your problem
+    result = torque_tot + C @ q_dot + r
+    return result
+
+
+
 
 def calculateInvDynamics(joint_positions, joint_velocities, joint_accelerations, robot_id):
     num_joints = len(joint_positions)
@@ -70,6 +79,19 @@ def compute_coriolis_matrix_v2(joint_positions, joint_velocities, joint_accelera
 
     return C
 
+    
+
+def calculateGravityMatrix(robot, joint_positions,joint_velocities,joint_accelerations):
+    num_joints = p.getNumJoints(robot)
+    # joint_velocities = [0.0] * num_joints  # Zero joint velocities
+    # joint_accelerations = [0.0] * num_joints  # Zero joint accelerations
+
+    joint_forces = calculateInvDynamics(joint_positions, joint_velocities, joint_accelerations,robot)
+    
+    gravity_matrix = np.array(joint_forces)
+    
+    return gravity_matrix
+
 
 def main():
 
@@ -90,6 +112,7 @@ def main():
 
     # Get the number of joints
     num_joints = p.getNumJoints(robot)
+    print(num_joints)
 
     # Give the final orientation and joint positions of the robot
     final_orientation = p.getQuaternionFromEuler([3.14, 0, 0])
@@ -132,61 +155,38 @@ def main():
         #print(f"Joint dofs: {joint_dof}")
        
         fixed_joints = joint_dof.count(-1)
-        joint_velocities = joint_velocities[:-fixed_joints]
-        joint_positions = joint_positions[:-fixed_joints]
+        #joint_velocities = joint_velocities[:-fixed_joints]
+        #joint_positions = joint_positions[:-fixed_joints]
 
         #print(f"Joint positions: {joint_positions}")
         #print(f"Joint velocities: {joint_velocities}")
 
-        inertia_matrix = p.calculateMassMatrix(robot,joint_positions)
-        #print("Inertia: ",np.array(inertia_matrix).shape)
-         
+        ###############################################################
+        # Retrieve the inertia matrix for each joint
+        inertia_matrices = []
+        for joint_index in range(num_joints):
+            dynamics_info = p.getDynamicsInfo(robot, joint_index)
+            inertia_matrix = list(dynamics_info[2])
 
-        # ###############################################################
-        # # Retrieve the inertia matrix for each joint
-        # inertia_matrices = []
-        # for joint_index in range(num_joints):
-        #     dynamics_info = p.getDynamicsInfo(robot, joint_index)
-        #     inertia_matrix = list(dynamics_info[2])
+            while len(inertia_matrix) < num_joints:
+                inertia_matrix.append(0)
 
-        #     while len(inertia_matrix) < num_joints:
-        #         inertia_matrix.append(0)
+            inertia_matrix = np.array(inertia_matrix)
 
-        #     inertia_matrix = np.array(inertia_matrix)
+            inertia_matrices.append(inertia_matrix)
 
-        #     inertia_matrices.append(inertia_matrix)
-
-        # inertia_matrices = np.array(inertia_matrices)
+        inertia_matrices = np.array(inertia_matrices)
+        #print("Inertia: ",np.array(inertia_matrices).shape)
         # joint_velocities = np.transpose(joint_velocities)
 
         ################################################################
 
         # Compute p(t) as M * q_dot
-        print("Calculating momentum")
-        p_t = np.dot(inertia_matrix, np.transpose(joint_velocities))
+        #print("Calculating momentum")
+        p_t = np.dot(inertia_matrices, np.transpose(joint_velocities))
         #print(f"p(t): {p_t}")
-
-       
-
-
-        # ###############################################################
-
-        # # Compute the Coriolis matrix
-        # print("Computing coriolis matrix")
-        # zero_accelerations = np.zeros(num_joints)
-        # print(zero_accelerations)
-
-
-        # coriolis_matrix = p.calculateInverseDynamics(robot, joint_positions, joint_velocities, zero_accelerations)
-
-        # print(f"Coriolis matrix: {coriolis_matrix}")
-        # ################################################################
-        # # Compute the gravity matrix
-        # zero_velocities = np.zeros(num_joints)
-        # print("Computing gravity matrix")
-        # gravity_matrix = p.calculateInverseDynamics(robot, joint_positions, zero_velocities, zero_accelerations)
-        
-        # print(f"Gravity matrix: {gravity_matrix}")
+        if i == 0: 
+            p_0 = p_t
         ################################################################
         # Compute joint accelerations using finite difference method
         dt = 1.0 / 240.0  # Time step
@@ -194,31 +194,71 @@ def main():
 
         joint_accelerations = np.transpose(joint_accelerations)
 
-        print(f"Joint accelerations: {joint_accelerations}")
+        #print(f"Joint accelerations: {joint_accelerations}")
 
         prev_joint_velocities = joint_velocities
 
+        #print("compute_coriolis_matrix")
         C = compute_coriolis_matrix_v2(joint_positions, joint_velocities, joint_accelerations,robot)
-        print(C)
-        print(C.shape)
+        #print(C)
+        #print(C.shape)
 
         ################################################################
 
+        # ################################################################
+        # # Compute the gravity matrix
+        # zero_velocities = np.zeros(num_joints)
+        #print("Computing gravity matrix")
+        gravity_matrix = calculateGravityMatrix(robot, joint_positions,joint_velocities, joint_accelerations)
+        
+        #print(f"Gravity matrix: {gravity_matrix}")
+       # print(gravity_matrix.shape)
 
+        #################### Algorithm ################################
+        
+        #Computing torque
+        tau = np.dot(inertia_matrices,joint_accelerations) + np.dot(C,joint_velocities) + gravity_matrix
+        #print('Torque:', tau)
+
+        # Create the diagonal gain matrix using np.diag
+        gain_values = [1.0, 0.5, 0.8,1.0, 0.5, 0.8,1.0, 0.5, 0.8]  # Example gain values for three joints
+        gain_matrix = np.diag(gain_values)
+
+        # Specify the integration limits
+        a = 0
+        b = i
+        r = p_t - p_0
+        #print ("RESULT = ",r)
+        # Call the quad function to perform integration
+        #result, error = quad(integrand, a, b, args=(b, joint_positions, joint_velocities, tau, C, result))
+
+        # Print the result
+        #print("Integration result:", result)
+        #print("Estimated error:", error)
+
+        
         # This is just a placeholder to generate a collision at a random link of the robot
-        if i == 20:
+        if i == 4:
             # Retrieve the link positions and orientations
             link_states = p.getLinkStates(robot, range(p.getNumJoints(robot)))
 
             link_positions = [state[0] for state in link_states]
             link_orientations = [state[1] for state in link_states]
 
+            print(len(link_positions))
             # Generate a random index to select a link
-            random_index = random.randint(0, len(link_positions))
-
+            random_index = random.randint(3, len(link_positions)-1)
+            #print(random_index)
             # Get the position and orientation of the randomly selected link
             selected_link_position = link_positions[random_index]
             selected_link_orientation = link_orientations[random_index]
+
+
+            #random_joint_index = p.getRandomJointIndices(robot, 1)[0]  # Get a random joint index
+            force = [400, 550, 700]  # Define the force vector (e.g., 10 N in the x-direction)
+            position = [0, 0, 0]  # Define the position of the external force (e.g., at the joint's origin)
+            p.applyExternalForce(robot, random_index, force, position, p.LINK_FRAME)  # Apply the force
+            print("FORCE APPLIED AT LINK" , random_index)
 
             # Create a visual marker at the selected point
             marker_size = 0.05
